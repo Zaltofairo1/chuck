66dd4c9fe6dc4509d5394cc28e6914339e8b300b
sergio altozano <sergio2006ap@gmail.com>
2021-05-17 17:46:09 +0200

    a

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from re import search
 import aiohttp
 import discord
 from discord import channel
@@ -5,8 +6,10 @@ from discord import message
 from discord.flags import Intents
 from dotenv import load_dotenv
 import os
+import random
 
 api_chuck = "https://api.chucknorris.io/jokes/random"
+api_search_chuck = "https://api.chucknorris.io/jokes/search?query="
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN_DS")
@@ -14,11 +17,6 @@ GUILD = int(os.getenv("GUILD_DS"))
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
-print(os.getenv("USERNAME"))
-
-@client.event
-async def on_ready():
-    print ("Ready!!!")
 
 @client.event
 async def on_message(message):
@@ -30,5 +28,15 @@ async def on_message(message):
                 if response.status == 200:
                     js = await response.json()
                     await message.channel.send(js["value"])
+
+    if message.content.startswith("€search "):
+        search_term = message.content[8:]
+        busqueda = api_search_chuck+search_term
+        async with aiohttp.ClientSession() as session:
+            async with session.get(busqueda) as response:
+                if response.status == 200:
+                    js = await response.json()
+                    await message.channel.send(random.choices(js["result"])[0]["value"])
+                    
 
 client.run(TOKEN)
